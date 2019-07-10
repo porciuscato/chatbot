95aa06037c424c0e5a9fdea9cd0f950bfe0db2b5
Jinhong Park <mpcato@naver.com>
2019-07-10 09:52:08 +0900

    changed

---
+++ 문제풀이용.py
@@ -0,0 +1,7 @@
+# a=[]
+# for i in range(0,5):
+#     a.append(int(input()))
+#     if a[i] >= 60:
+#         print("{}번 학생은 {}점으로 합격입니다.".format(i,a[i]))
+#     else :
+#         print("{}번 학생은 {}점으로 불합격입니다.".format(i,a[i]))
